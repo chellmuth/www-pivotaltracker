2f67ef646bc7ab00042df2be60e511e7b853cd0f
Jacob Helwig <jacob@technosorcery.net>
2008-12-16 22:02:09 -0800

    Initial commit of base files.

---
+++ lib/WWW/PivotalTracker.pm
@@ -0,0 +1,107 @@
+package WWW::PivotalTracker;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+WWW::PivotalTracker - The great new WWW::PivotalTracker!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use WWW::PivotalTracker;
+
+    my $foo = WWW::PivotalTracker->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Jacob Helwig, C<< <jhelwig at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-www-pivotaltracker at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-PivotalTracker>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc WWW::PivotalTracker
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WWW-PivotalTracker>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WWW-PivotalTracker>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WWW-PivotalTracker>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WWW-PivotalTracker/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Jacob Helwig, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of WWW::PivotalTracker
