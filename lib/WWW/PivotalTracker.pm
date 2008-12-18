740587b74e01865a9034f6b0ef12cdffd8f3eb54
Jacob Helwig <jacob@technosorcery.net>
2008-12-17 21:14:41 -0800

    Basic ability to query project details, and create new stories.

--- lib/WWW/PivotalTracker.pm
+++ lib/WWW/PivotalTracker.pm
@@ -3,6 +3,23 @@ package WWW::PivotalTracker;
 use warnings;
 use strict;
 
+use parent 'Exporter';
+
+use Perl6::Parameters;
+
+use aliased 'HTTP::Message';
+use aliased 'HTTP::Request';
+use aliased 'LWP::UserAgent';
+use aliased 'XML::Tidy';
+
+use Carp qw/
+    croak
+/;
+use XML::Simple qw/
+    XMLin
+    XMLout
+/;
+
 =head1 NAME
 
 WWW::PivotalTracker - The great new WWW::PivotalTracker!
@@ -18,13 +35,14 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+This module provides simple methods to interact with the Pivotal Tracker API.
 
-Perhaps a little code snippet.
+    use WWW::PivotalTracker qw/
+        project_details
+    /;
 
-    use WWW::PivotalTracker;
+    my $details = project_details("API Token", "Project ID");
 
-    my $foo = WWW::PivotalTracker->new();
     ...
 
 =head1 EXPORT
@@ -32,20 +50,157 @@ Perhaps a little code snippet.
 A list of functions that can be exported.  You can delete this section
 if you don't export anything, such as for a purely object-oriented module.
 
+=cut
+
+our @EXPORT_OK = qw/
+    add_story
+    project_details
+/;
+
 =head1 FUNCTIONS
 
-=head2 function1
+=head2 project_details
+
 
 =cut
 
-sub function1 {
+sub project_details($token, $project_id) {
+    croak("Malformed Project ID: '$project_id'") unless __PACKAGE__->_check_project_id($project_id);
+
+    my $response = __PACKAGE__->_do_request($token, "projects/$project_id", "GET");
+
+
+    if (!defined $response || $response->{'success'} ne 'true') {
+        return {
+            success => 'false',
+            reason  => 'Epic fail!',
+        };
+    }
+
+    return {
+        success         => 'true',
+        iteration_weeks => $response->{'project'}->{'iteration_length'}->{'content'},
+        name            => $response->{'project'}->{'name'},
+        point_scale     => $response->{'project'}->{'point_scale'},
+        start_day       => $response->{'project'}->{'week_start_day'},
+    };
 }
 
-=head2 function2
+=head2 add_story
 
 =cut
 
-sub function2 {
+sub add_story($token, $project_id, $story_details)
+{
+    croak("Malformed Project ID: '$project_id'") unless __PACKAGE__->_check_project_id($project_id);
+
+    use Data::Dumper;
+    foreach my $key (keys %$story_details) {
+        croak("Unrecognized option: $key")
+            unless _is_one_of($key, [qw/
+                created_at
+                current_state
+                description
+                estimate
+                labels
+                name
+                note
+                requested_by
+                story_type
+            /]);
+    }
+    croak("Name is required for a new story") unless exists $story_details->{'name'};
+    croak("Requested By is required for a new story") unless exists $story_details->{'requested_by'};
+
+    my $content = __PACKAGE__->_make_xml({ story => $story_details });
+
+    my $response = __PACKAGE__->_do_request($token, "projects/$project_id/stories", "POST", $content);
+
+    if (!defined $response || $response->{'success'} ne 'true') {
+        return {
+            success => 'false',
+            errors  => $response->{'errors'},
+        };
+    }
+
+    my $story = $response->{'story'}->[0];
+    return {
+        success       => 'true',
+        id            => $story->{'id'}->{'content'},
+        name          => $story->{'name'},
+        description   => $story->{'description'},
+        estimate      => $story->{'estimate'}->{'content'},
+        current_state => $story->{'current_state'},
+        created_at    => $story->{'created_at'},
+        story_type    => $story->{'story_type'},
+        requested_by  => $story->{'requested_by'},
+        labels        => (exists $story->{'labels'} ? $story->{'labels'}->{'label'} : undef),
+        url           => $story->{'url'},
+    };
+}
+
+sub _check_project_id($class, $project_id)
+{
+    return $project_id =~ m/^\d+$/ ? 1 : 0;
+}
+
+sub _is_one_of($element, $set)
+{
+    return((scalar grep { $_ eq $element } @$set) ? 1 : 0);
+}
+
+sub _do_request($class, $token, $request_url, $request_method; $content)
+{
+    my $base_url = "https://www.pivotaltracker.com/services/v1/";
+
+    my $request = Request->new(
+        $request_method,
+        $base_url . $request_url,
+        [
+            'X-TrackerToken' => $token,
+            'Content-type'   => 'application/xml',
+        ],
+        $content
+    );
+
+    my $response = __PACKAGE__->_post_request($request);
+    croak($response->status_line()) unless ($response->is_success());
+
+    print $response->content();
+
+    return XMLin(
+        $response->content(),
+        ForceArray => [qw/
+            error
+            iteration
+            label
+            note
+            story
+        /],
+        GroupTags => {
+            errors => 'error',
+            labels => 'label',
+        },
+        KeyAttr => [],
+        SuppressEmpty => undef,
+    );
+}
+
+sub _post_request($class, $request)
+{
+    my $ua = UserAgent->new();
+
+    return $ua->request($request);
+}
+
+sub _make_xml($class, HASH $data)
+{
+    return XMLout(
+        $data,
+        AttrIndent => 0,
+        KeepRoot => 1,
+        NoAttr => 1
+    );
 }
 
 =head1 AUTHOR
